cbc0fb5085c79263abfadda2c34a18f04cf44023
biangomes <beanascigom@gmail.com>
2023-05-11 13:18:34 -0300

    uso de bloco switch-case

---
+++ estrutura-controle/20_switch_case.js
@@ -0,0 +1,32 @@
+const diaSemana = 1;
+
+switch (diaSemana) {
+    case 1: {
+        console.log("case 1 com um bloco de código");
+        console.log("vários console.log");
+        console.log("domingo");
+        break;
+    }
+    case 2:
+        console.log("segunda");
+        break;
+    case 3:
+        console.log("terça");
+        break;
+    case 4:
+        console.log("quarta");
+        break;
+    case 5:
+        console.log("quinta");
+        break;
+    case 6:
+        console.log("sexta");
+        break;
+    case 7:
+        console.log("sábado");
+        break;
+
+    default:
+        console.log("dia da semana inválido");
+        break;
+}
