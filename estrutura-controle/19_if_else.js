2595a68b1b8c3d92faa1f298f337b783fa6158ea
biangomes <beanascigom@gmail.com>
2023-05-11 13:09:54 -0300

    Formas diferentes de utilizar if-else

--- estrutura-controle/19_if_else.js
+++ estrutura-controle/19_if_else.js
@@ -1,7 +1,17 @@
-const nota = 7;
+const nota = 5;
 
 if (nota >= 7) {
     console.log("Aprovado");
+} else if (nota < 7 && nota >= 5) {
+    console.log("recuperação");
 } else {
     console.log("Reprovado");
 }
+
+// outra forma de utilizar o bloco de código acima
+if (nota >= 7) console.log("aprovado!")
+else console.log("reprovado");
+
+
+// if ternario
+console.log(nota >= 7 ? "Aprovado" : "Reprovado");
